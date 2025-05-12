f21c56854414eabd6e37502b512387fb906e53b7
OxygenTheSmolAngel <ztwo5548@gmail.com>
2025-05-12 19:45:36 -0400

    Replaces the old Unown King front sprite with a new and remade sprite

--- gfx/pokemon/unownking/anim.asm
+++ gfx/pokemon/unownking/anim.asm
@@ -1,10 +1,12 @@
 	frame 0, 08
 	frame 1, 08
 	frame 0, 08
-	frame 1, 07
-	frame 2, 24
-	frame 3, 08
+	frame 1, 08
+	frame 0, 16
 	frame 4, 08
+	frame 2, 08
+	frame 3, 08
+	frame 2, 08
 	frame 3, 08
 	frame 4, 08
 	frame 0, 16
